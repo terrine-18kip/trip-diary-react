f23c6964cbb451a45e7282537be83bb0de1a8d07
terrine-18kip <73570560+terrine-18kip@users.noreply.github.com>
2022-05-12 22:13:17 +0900

    CSSファイル分割

--- frontend/src/components/SpotList.tsx
+++ frontend/src/components/SpotList.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { UserContext } from '../Context'
-import { Button } from 'antd'
-import { PlusOutlined, DeleteOutlined, MenuOutlined } from '@ant-design/icons'
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
 import SpotCreate from './SpotCreate'
 import SpotEdit from './SpotEdit'
 import axios from 'axios'
+import { styles } from '../styles/SpotList.styles'
+import { Button } from 'antd'
+import { PlusOutlined, DeleteOutlined, MenuOutlined } from '@ant-design/icons'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+/** @jsxImportSource @emotion/react */
 
 const apiUrl = process.env.REACT_APP_API_URL
 
@@ -95,83 +95,6 @@ const SpotList: React.FC<Props> = ({ plan, getTrip }) => {
     } catch (error) {
       console.log(error)
     }
-  }
-
-  const styles = {
-    spots: css`
-      width: 100%;
-      margin-bottom: 10px;
-    `,
-    spot: css`
-      padding: 5px 10px;
-      border-bottom: 1px solid #eaeaea;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      cursor: pointer;
-      transition: background 0.2s;
-      background-color: rgba(250, 250, 250, 0.7);
-      @media screen and (max-width: 768px) {
-        padding: 5px 1.5%;
-      }
-      &:hover {
-        background-color: #f5f5f5;
-      }
-    `,
-    spotDrag: css`
-      margin-right: 5px;
-      font-size: 1.2em;
-      &:hover {
-        cursor: grab;
-      }
-    `,
-    spotTime: css`
-      width: 45px;
-      font-size: 12px;
-      text-align: center;
-      p {
-        margin: 0;
-        line-height: 1;
-      }
-    `,
-    spotCategory: css`
-      width: 30px;
-      height: 30px;
-      margin: 0 10px;
-      border-radius: 100vh;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background-color: #f5f5f5;
-      @media screen and (max-width: 768px) {
-        margin: 0 1.5%;
-      }
-      img {
-        width: 24px;
-      }
-    `,
-    spotName: css`
-      width: 100%;
-      margin-bottom: 0;
-      flex: 1;
-    `,
-    spotFee: css`
-      width: 70px;
-      font-size: 13px;
-      text-align: right;
-      white-space: nowrap;
-    `,
-    spotDelete: css`
-      padding: 0 5px;
-      text-align: center;
-    `,
-    form: css`
-      margin-bottom: 20px;
-      text-align: center;
-    `,
-    disabled: css`
-      cursor: auto;
-    `,
   }
 
   return (
--- frontend/src/components/TripDetail.tsx
+++ frontend/src/components/TripDetail.tsx
@@ -4,6 +4,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom'
 import TripMember from './TripMember'
 import SpotList from './SpotList'
 import axios from 'axios'
+import { styles } from '../styles/TripDetail.styles'
 import { PageHeader, Card, Avatar, Space, Button, Form, InputNumber } from 'antd'
 import {
   FormOutlined,
@@ -13,9 +14,7 @@ import {
   UserOutlined,
   InfoCircleOutlined,
 } from '@ant-design/icons'
-import moment from 'moment'
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
 
 const apiUrl = process.env.REACT_APP_API_URL
 
@@ -187,71 +186,6 @@ const TripDetail: React.FC = () => {
     if (event.key === 'Enter') {
       updatePlan(id, old, event.currentTarget.value)
     }
-  }
-
-  const styles = {
-    container: css`
-      width: 100%;
-      min-height: 100vh;
-      padding: 10px 20px;
-      background-color: #fafafa;
-      @media screen and (max-width: 768px) {
-        padding: 5px 1%;
-      }
-    `,
-    tripMembers: css`
-      display: flex;
-      align-items: center;
-      flex-wrap: wrap;
-    `,
-    tripMember: css`
-      display: flex;
-      align-items: center;
-      margin-right: 10px;
-      padding: 2px 0;
-    `,
-    plans: css`
-      padding: 20px 10px;
-      @media screen and (max-width: 768px) {
-        padding: 15px 1%;
-      }
-    `,
-    plan: css`
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      padding: 2px 10px;
-      border-radius: 2px;
-      background-color: #ecd4c2;
-    `,
-    planNum: css`
-      margin: 0;
-      cursor: pointer;
-    `,
-    planForm: css`
-      width: 80%;
-      margin: 0 auto;
-    `,
-    planInput: css`
-      width: 55px;
-      background-color: #fff;
-    `,
-    disabled: css`
-      cursor: auto;
-    `,
-    unauthorized: css`
-      height: calc(100vh - 100px);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      p {
-        margin-left: 10px;
-        margin-bottom: 0;
-        color: #555;
-        font-size: 16px;
-        font-weight: 500;
-      }
-    `,
   }
 
   const dailyElement = (plan: Plan): JSX.Element => {
