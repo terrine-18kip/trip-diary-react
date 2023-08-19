76bd0aee95de63770d360d5667703c43ac3de0d1
terrine-18kip <ryota.meiwa.3618@gmail.com>
2023-08-19 17:28:24 +0900

    プライバシーポリシーページ作成

---
+++ frontend/src/components/pages/PrivacyPolicy/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PrivacyPolicy: React.FC = () => {
+  return (
+    <div>プライバシーポリシー</div>
+  )
+}
+
+export default PrivacyPolicy
---
+++ frontend/src/components/pages/Terms/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Terms: React.FC = () => {
+  return (
+    <div>利用規約</div>
+  )
+}
+
+export default Terms
